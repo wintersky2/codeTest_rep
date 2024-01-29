3856108a73e4232d7a0750b98607e8f135f023e1
wintersky2 <emsw747@gmail.com>
2024-01-29 15:10:09 +0900

    가장 많이 받은 선물

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import java.util.HashMap;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String[] friends = {"muzi", "ryan", "frodo", "neo"};
+        String[] gifts = {"muzi frodo", "muzi frodo", "ryan muzi", "ryan muzi", "ryan muzi", "frodo muzi", "frodo ryan", "neo muzi"};
+
+        HashMap<String,Integer> firstQuestion = new HashMap<>();
+        for(int i = 0; i < friends.length; i++){
+            for(int j = i; j<friends.length-1;j++){
+                firstQuestion.put(friends[i]+" "+friends[j+1],0);
+            }
+        }
+        System.out.println(firstQuestion.toString());
     }
 }
